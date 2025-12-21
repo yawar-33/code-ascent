be36dce5303b90d6050d346ee15716b14cd9b0e7
Yawar Ali <apple@Yawar-Ali.local>
2025-12-21 21:45:54 +0500

    Deployment

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { projects } from '@/data/data';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://codeascent.org'; // Replace with actual domain
 
